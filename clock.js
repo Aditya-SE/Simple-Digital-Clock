83d3ced2f0769cbe8525285fc778b43fe6b5843a
Aditya-SE <adityapatil849703801313@gmail.com>
2025-10-12 21:08:40 +0530

    updated the file

--- clock.js
+++ clock.js
@@ -1,7 +1,7 @@
-let hourEl = document.getElementById("hour");
-let minuteEl = document.getElementById("minutes");
-let secondEl = document.getElementById("seconds");
-let ampmEl = document.getElementById("ampm");
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
 
 function updateClock() {
     let h= new Date().getHours();
@@ -13,4 +13,15 @@ function updateClock() {
         h = h - 12;
         ampm = "PM";
     }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hourEl.innerText = h;
+    minuteEl.innerText = m;
+    secondEl.innerText = s;
+    ampmEl.innerText = ampm;
+    setTimeout(updateClock , 1000);
 }
+updateClock();
